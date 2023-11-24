95a394e4ba21f00ad18c3e5695e5fc1f4ee71249
adarsh <29173346+akxcix@users.noreply.github.com>
2023-11-25 03:26:08 +0530

    Update About.tsx

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -19,13 +19,11 @@ const About: FC<AboutProps> = ({ className }: AboutProps) => {
                 </H1>
                 <br />
                 <P>
-                    hey! i am adarsh
-                    <br />
-                    i don't really know why i created this site, but i am bored so might as well
-                    get it over with. i am a software engineer currently working at flipkart. apart
-                    from the work stuff i occasionally like to play with dogs, follow startups
-                    rant on twitter and click photographs. if you are interested in those, you
-                    would find the links in the footer
+                    hey! i am adarsh<br />
+                    &gt; a ramblin' wreck in some latent space<br />
+                    &gt; prev: @flipkart @askfilo<br />
+                    &gt; n&w s4 @_buildspace<br />
+                    &gt; TIET '21
                 </P>
             </Column>
         </div>
